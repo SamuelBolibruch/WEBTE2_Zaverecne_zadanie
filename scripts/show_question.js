9e5c113db374c42751b2952c82853b1a326ce829
xpuvak <xpuvak@stuba.sk>
2024-05-20 19:53:24 +0200

    - final

--- scripts/show_question.js
+++ scripts/show_question.js
@@ -1,6 +1,6 @@
 document.getElementById('codeForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const code = document.getElementById('codeInput').value;
-    const baseURL = "https://node104.webte.fei.stuba.sk/";
+    const baseURL = "https://node97.webte.fei.stuba.sk/";
     window.location.href = baseURL + code;
 });
